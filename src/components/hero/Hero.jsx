7aeff4da7a78af17ea99ceda8bbf8f97423d0b95
micalo-mica <nwaforchukwukamicah@gmail.com>
2023-07-06 22:06:21 +0100

    hero add

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,7 +5,16 @@ const Hero = () => {
   const { closeSubmenu } = useGlobalContext();
   return (
     <div className="hero" onMouseOver={closeSubmenu}>
-      Hero
+      <div className="heroContainer">
+        <h2 className="title">
+          The propose of this project is to build strip navbar and sidebar
+        </h2>
+        <p>
+          Millions of companies of all sizes—from startups to Fortune 500s—use
+          Stripe’s software and APIs to accept payments, send payouts, and
+          manage their businesses online.
+        </p>
+      </div>
     </div>
   );
 };
